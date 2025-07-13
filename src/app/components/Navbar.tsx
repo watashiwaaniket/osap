51dc9b1b7425c3f70eae6e9843aa2114feb3647a
watashiwaaniket <aniketdhakane3@gmail.com>
2025-07-13 23:15:03 +0530

    added a model, might or might not use it

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,12 +6,11 @@ import { Button } from "./Button";
 export function Navbar(){
     //design a beautiful navbar, this is just a placeholder rn
     const session = useSession();
+
     return(
         <div className="flex p-2 border rounded-xl m-4 justify-end">
             {!session.data?.user ? <Button text="login" variant="primary" size="sm" animated={false} glow={true} onClick={() => {signIn()}}/> : null}
             {session.data?.user ? <Button text="logout" variant="primary" size="sm" animated={false} glow={true} onClick={() => {signOut()}} /> : null}
-            
-            
         </div>
     )
 }
