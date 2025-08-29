7e205482bc7b12995fe304967adce7fa5f605535
watashiwaaniket <aniketdhakane3@gmail.com>
2025-08-29 21:55:14 +0530

    added osap icon

---
+++ src/app/lib/icons/index.ts
@@ -0,0 +1,10 @@
+export interface IconProps {
+    size : "sm" | "md" | "lg",
+    color ?: string
+}
+
+export const iconSizeVariants = {
+    "sm" : "size-3",
+    "md" : "size-4",
+    "lg" : "size-5",
+}
